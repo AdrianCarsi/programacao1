fe0cd0595f84f148e40a0bba5e762a2165c9fac3
AdrianCarsi <106412060+AdrianCarsi@users.noreply.github.com>
2022-06-09 19:47:00 -0300

    Create vogal.c

---
+++ vogal.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+
+char entrada;
+scanf("%c", &entrada);
+switch (entrada) {
+
+case 'A':
+printf("Vogal");
+break;
+case 'E':
+printf("Vogal");
+break;
+case 'I':
+printf("Vogal");
+break;
+case 'O':
+printf("Vogal");
+break;
+case 'U':
+printf("Vogal");
+break;
+case 'a':
+printf("Vogal");
+break;
+case 'e':
+printf("Vogal");
+break;
+case 'i':
+printf("Vogal");
+break;
+case 'o':
+printf("Vogal");
+break;
+case 'u':
+printf("Vogal");
+break;
+default:
+printf("Vocẽ não digitou nenhuma vogal");
+} 
+
+}
