0e703a84d66d84ae6295993dbf1caa5f3c5be4dd
AdrianCarsi <106412060+AdrianCarsi@users.noreply.github.com>
2022-06-09 14:54:17 -0300

    Update atividade2.c

--- Exercícios_de_sala/atividade2.c
+++ Exercícios_de_sala/atividade2.c
@@ -41,14 +41,56 @@ printf("%i , %s\n , %f\n , %i\n ", meuNumero, meuCaractere, meuDecimal, meuNumer
    return 0;
 }
 
-
-A soma de 4 e 9 é 13.
-
 b)
-=> o produto de 5 e 5 é 25.
+   int main() {
+   int n1, p;
+   n1=5;
+   p=n1*n1;
+   printf("O PRODUTO É: %i", p); 
+   return 0;
+}
+
 
 c)
-Opcao 'A': 24, opcao 'B': 30.
-
+int main() {
+   float n1, n2, s, p;
+   n1=22.6770;
+   n2=1.3229;
+   p=n1*n2;
+   s= n1+n2;
+   printf("SOMA É: %f", s);
+   printf("O PRODUTO É: %f", p);
+   return 0;
+}
+    
 d)
-[caractere digitado: "B"]
+[caractere digitado: "B"] ( não entendi a pergunta)
+ 
+5.
+    
+int main() {
+int x = 5;
+int y = 10;
+printf("%d", x + y);
+return 0;
+    }
+
+6.
+    int main() {
+int x = 5 , y = 6 , z = 50;
+printf("%d", x + y + z);
+return 0;
+    }
+
+7.
+int meuNumero = 5;
+float meuNumeroDecimal = 5.99;
+char meuCaractere = 'D';
+
+8.
+int main() {
+int x = 10;
+int y = 5;
+printf("%i", "x * y", x , y);
+return 0;
+    }
