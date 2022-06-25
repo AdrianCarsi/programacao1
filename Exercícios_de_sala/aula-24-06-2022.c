9abfb690623cb3660f0fc7e7d0439457757ca4b7
AdrianCarsi <106412060+AdrianCarsi@users.noreply.github.com>
2022-06-24 21:35:01 -0300

    Update aula-24-06-2022.c

--- Exercícios_de_sala/aula-24-06-2022.c
+++ Exercícios_de_sala/aula-24-06-2022.c
@@ -14,3 +14,40 @@ printf("Não está equilibrado\n");
     return 0;
 }
 
+#include <stdio.h>
+
+int main(){
+int N, M, B;
+printf("Número de bolinhas de chocolate:\n");
+scanf("%d",&N);
+printf("Número de bolinhas de chocolate que serão comidas por vez:\n");
+scanf("%d",&M);
+while(N>0){
+    if(B==0){
+        printf("Paula - ");
+        B=1;
+}
+else{
+    printf("Carlos - ");
+    B=0;
+ 
+}
+N=N-M;
+if(N<0){
+    printf("0\n");
+}
+else{
+printf("%d\n",N);
+}
+}
+if(B==0)
+printf("Carlos ganhou\n");
+else
+printf("Paula ganhou\n");
+
+
+
+
+
+    return 0;
+}
