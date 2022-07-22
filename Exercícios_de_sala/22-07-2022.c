ddbd6651155c8604ba226e2426eebd7211c0a475
AdrianCarsi <106412060+AdrianCarsi@users.noreply.github.com>
2022-07-22 20:38:18 -0300

    Update 22-07-2022.c

--- Exercícios_de_sala/22-07-2022.c
+++ Exercícios_de_sala/22-07-2022.c
@@ -14,3 +14,20 @@ int main(){
     printf("Nome:%s\nIdade:%d\n",var1.nome,var1.idade);
     return 0;
 }
+
+#include <stdio.h>
+
+typedef struct {
+        char nome[20];
+        int idade;
+}pessoa;
+
+int main(){
+    pessoa var1={"Adrian",19};
+    printf("Digite seu nome: ");
+    scanf("%s",var1.nome);
+    printf("Digite sua idade: ");
+    scanf("%d",var1.idade);
+    printf("Nome:%s\nIdade:%d\n",var1.nome,var1.idade);
+    return 0;
+}
