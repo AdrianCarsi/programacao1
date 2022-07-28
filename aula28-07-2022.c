da1f80da9b9e298be127d4093023dd6dff27728d
AdrianCarsi <106412060+AdrianCarsi@users.noreply.github.com>
2022-07-28 19:09:20 -0300

    Update aula28-07-2022.c

--- aula28-07-2022.c
+++ aula28-07-2022.c
@@ -28,9 +28,9 @@ int main(){
 void fibo(int n){
     int n1=1, n2=2, p, i;
     if(n<1) return;
-    printf("%d" ,n1);
+    printf("%d ",n1);
     for(i=0;i<n-1;i++){
-        printf("%d" ,n2);
+        printf("%d ",n2);
         p=n1+n2;
         n1=n2;
         n2=p;
